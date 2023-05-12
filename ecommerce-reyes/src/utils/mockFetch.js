a597ac26d58605785729c9b12ae8aa7d36c3bdca
alinareyesa <alinareyesa22@gmail.com>
2023-05-12 20:59:06 -0300

    ultima entrega

--- ecommerce-reyes/src/utils/mockFetch.js
+++ ecommerce-reyes/src/utils/mockFetch.js
@@ -1,11 +1,13 @@
 let products = [
-    {id: '1', category: 'choker', name: 'choker lolita', stock: 12, price: 260, image: '../src/assets/imagenes/chokerlolita.jpg'},
-    {id: '2', category: 'choker', name: 'choker goth', stock: 14, price: 250, image: '../src/assets/imagenes/chokergoth.jpg'},
-    {id: '3', category: 'headband', name: 'headband lolita', stock: 10, price: 450, image: '../src/assets/imagenes/headbandlolita.jpg'},
-    {id: '4', category: 'headband', name: 'headband goth', stock: 12, price: 450, image: '../src/assets/imagenes/headbandgoth.jpg'},
-    {id: '5', category: 'choker', name: 'choker moon', stock: 10, price: 300, image: '../src/assets/imagenes/chokermoon.jpg'},
-    {id: '6', category: 'headband', name: 'headband bunny', stock: 8, price: 500, image: '../src/assets/imagenes/bunnyheadband.jpg'}
-
+    {id: '1', category: 'choker', name: 'choker lolita', stock: 12, price: 260, image: '/public/images/chokerlolita.jpg'},
+    {id: '2', category: 'choker', name: 'choker goth', stock: 14, price: 250, image: '/public/images/chokergoth.jpg'},
+    {id: '3', category: 'headband', name: 'headband lolita', stock: 10, price: 450, image: '/public/images/headbandlolita.jpg'},
+    {id: '4', category: 'headband', name: 'headband goth', stock: 12, price: 450, image: '/public/images/headbandgoth.jpg'},
+    {id: '5', category: 'choker', name: 'choker moon', stock: 10, price: 300, image: '/public/images/chokermoon.jpg'},
+    {id: '6', category: 'headband', name: 'headband bunny', stock: 8, price: 500, image: '/public/images/bunnyheadband.jpg'},
+    {id: '7', category: 'headband', name: 'headband neko', stock: 10, price: 500, image: '/public/images/headbandneko.jpg'},
+    {id: '8', category: 'choker', name: 'choker neko', stock: 10, price: 280, image: '/public/images/chokerneko.jpg'},
+    {id: '9', category: 'choker', name: 'choker sanrio', stock: 10, price: 300, image: '/public/images/chokersanrio.jpg'}
 ]
 
 export let mockFetch = (id) => {
